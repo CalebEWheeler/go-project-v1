d065aad553a008ca2a6f392ab535d0f18ff8e35d
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-19 20:34:06 -0600

    feat: began using mux to connect to localhost:8080 and display text

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/CalebEWheeler/go-project-v1
 
 go 1.16
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
--- main.go
+++ main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/CalebEWheeler/go-project-v1/config"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-type User struct {
-	Name string `json:"name"`
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	//run with 'go run main.go
+	//kill with 'ctrl+c'
+	fmt.Fprintf(w, "Hello World")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", helloWorld).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
-	fmt.Println("Go MySQL Integration")
+	fmt.Println("Go ORM Basics")
 
-	db, err := sql.Open("mysql", config.MySQLCredentials())
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer db.Close()
-
-	results, err := db.Query("SELECT name FROM users")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for results.Next() {
-		var user User
-
-		err = results.Scan(&user.Name)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		fmt.Println(user.Name)
-	}
-
+	handleRequests()
 }
