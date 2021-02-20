98cfbffdf42f7204fc8638035b5bcf338a39497a
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-19 19:18:16 -0600

    feat: successfully connected to MySQL

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/CalebEWheeler/go-project-v1
 
 go 1.16
 
-require github.com/CalebEWheeler/go-project-v2 v0.0.0-20210219235058-2da070585aae
+require (
+	github.com/CalebEWheeler/go-project-v2 v0.0.0-20210219235058-2da070585aae
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
-	"github.com/CalebEWheeler/go-project-v2/config"
+	"github.com/CalebEWheeler/go-project-v1/config"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	println(config.Message())
+	fmt.Println("Go MySQL Integration")
+
+	db, err := sql.Open("mysql", config.MySQLCredentials())
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	fmt.Println("Successfully Connected to MySQL database with ignored config file")
 }
