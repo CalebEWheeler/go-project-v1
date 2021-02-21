9a42787622cb9ee4bef9db1f737ccd25f7e7f608
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-21 14:02:22 -0600

    feat: modify methods in person.go that handle CRD functionality with a passed person object

--- person/person.go
+++ person/person.go
@@ -1,6 +1,7 @@
 package person
 
 import (
+	"github.com/CalebEWheeler/go-project-v1/database"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 )
@@ -12,15 +13,28 @@ type Person struct {
 }
 
 func GetPeople(c *fiber.Ctx) {
-	c.Send("All People")
+	db := database.DBConn
+	var people []Person
+	db.Find(&people)
+	c.JSON(people)
 }
 
 func GetPerson(c *fiber.Ctx) {
-	c.Send("A Person")
+	id := c.Params("id")
+	db := database.DBConn
+	var person Person
+	db.Find(&person, id)
+	c.JSON(person)
 }
 
 func CreatePerson(c *fiber.Ctx) {
-	c.Send("Adds a Person")
+	db := database.DBConn
+	var person Person
+	person.Name = "Caleb"
+	person.Age = "26"
+
+	db.Create(&person)
+	c.JSON(person)
 }
 
 func UpdatePerson(c *fiber.Ctx) {
@@ -28,5 +42,13 @@ func UpdatePerson(c *fiber.Ctx) {
 }
 
 func DeletePerson(c *fiber.Ctx) {
-	c.Send("Delete a Person")
+	id := c.Params("id")
+	db := database.DBConn
+	var person Person
+	db.First(&person, id)
+	if person.Name == "" {
+		c.Status(500).Send("No person found with given ID")
+	}
+	db.Delete(&person)
+	c.Send("Person successfully deleted")
 }
