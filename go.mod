18260d15eb8d714e7818e4ea0554929eea6a29fd
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-21 11:10:55 -0600

    feat: create a new fiber app that will utilize helloWorld() to display a message on port :8080

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/CalebEWheeler/go-project-v1
 go 1.16
 
 require (
+	github.com/gofiber/fiber v1.14.6
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/gofiber/fiber"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	//run with 'go run main.go
-	//kill with 'ctrl+c'
-	fmt.Fprintf(w, "Hello World")
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/users", AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/create", NewUserForm).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{id}", NewUser).Methods("POST")
-	myRouter.HandleFunc("/user/{id}", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/update", UpdateUserForm).Methods("GET")
-	myRouter.HandleFunc("/user/update", UpdateUser).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, World!")
 }
 
 func main() {
-	fmt.Println("Go ORM Basics")
+	app := fiber.New()
 
-	InitialMigration()
+	app.Get("/", helloWorld)
 
-	handleRequests()
+	app.Listen(8080)
 }
